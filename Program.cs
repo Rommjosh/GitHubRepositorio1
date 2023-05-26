3a252b005dfa791b6afde10857bcc47617c65241
Rommjosh <133920042+Rommjosh@users.noreply.github.com>
2023-05-25 18:46:45 -0600

    feat se quitó el ordenamiento
     No es necesario ordenar para encontrar el mayor y el menor

--- Program.cs
+++ Program.cs
@@ -27,10 +27,6 @@ Console.WriteLine();
 //Mostrar resultado
 for (var i = 0; i < Poblacion.Length; i++)
     Console.WriteLine($"{Departamento[i],20} ==> {Poblacion[i],10:N0}");
-
-
-
-
 //Sumar todas las poblaciones con sum de LinQ
 Console.WriteLine($"Población General:{diccionario.Values.Sum():N0}");
 Console.WriteLine($"Departamento con mayor Población:{maxDepkey}");
